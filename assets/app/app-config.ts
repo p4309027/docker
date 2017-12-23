eff64736f1545a47cdc73678f49bf41ddb7dfa05
p4309027 <p4309027@tees.ac.uk>
2017-12-23 19:47:10 +0000

    kept only deployment files

--- assets/app/app-config.ts
+++
@@ -1,6 +0,0 @@
-import { Headers} from "@angular/http";
-
-export const appConfig = {
-    apiUrl: "http://3amigosa.azurewebsites.net/as/api/",
-    header: new Headers({'Content-Type': 'application/json'})
-}
--- assets/app/models/address.model.ts
+++
@@ -1,8 +0,0 @@
-export class AddressModel {
-    constructor(public houseNumber: string,
-                public street: string,
-                public city: string,
-                public postcode:string,
-                public userId?: string,
-                public _id?:string) {}
-}
